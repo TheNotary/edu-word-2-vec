b9678fb5b474159a7ddf2d417bfe786abbe66140
TheNotary <no@email.plz>
2023-05-16 19:20:43 -0500

    pairing

--- README.md
+++ README.md
@@ -5,3 +5,14 @@
 ```
 pip install --upgrade gensim
 ```
+
+## Pairing Intention
+
+Solve these simple problems:
+
+- king - man = X (L: queen)
+- king + queen = x (N: jibberish? L: emperor, god?)
+- (king - man) + king = x (N: emperor, L: emperor)
+- king + peasant = x
+- king - peasant =
+-
--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from gensim.models.word2vec import Word2Vec
 
 embeds = api.load("glove-wiki-gigaword-50") # 66MB
 
-v1 = embeds['good']
-v2 = embeds['morning']
-gm = v1 + v2
+v1 = embeds['king']
+v2 = embeds['man']
+v3 = embeds['woman']
+v4 = embeds['queen']
+
+gm = v1 + v4
 sims = embeds.similar_by_vector(gm)
 
 print(sims)
-
